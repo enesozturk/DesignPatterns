b586f91e07c982b529e405ad0f8c57dac6541c35
Enes <enesozturk.dev@gmail.com>
2017-08-27 19:17:32 +0300

    Singleton
    singleton design pattern

---
+++ Singleton/Singleton/Policy.cs
@@ -0,0 +1,23 @@
+﻿namespace Singleton
+{
+    public class Policy
+    {
+        private static readonly Policy _instance = new Policy();
+        public static Policy Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public Policy()
+        {
+
+        }
+        public int Id { get; set; } = 123;
+        public string Insured { get; set; } = "Enes Ozturk";
+
+        public string GetInsuredName() => Insured;
+    }
+}
