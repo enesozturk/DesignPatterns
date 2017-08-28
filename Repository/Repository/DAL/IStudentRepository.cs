41edf1656a8f699c32de323a17ba55c9b0af8486
Enes <enesozturk.dev@gmail.com>
2017-08-28 12:59:13 +0300

    Repository and Unif Of Work
    repository and unit of work design patterns

---
+++ Repository/Repository/DAL/IStudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Repository.DAL
+{
+    public interface IStudentRepository : IDisposable
+        {
+            Task<List<Student>> GetStudents();
+            Task<Student> GetStudentByID(int? studentId);
+            void InsertStudent(Student student);
+            void DeleteStudent(int studentID);
+            void UpdateStudent(Student student);
+            Task<int> Save();
+        } 
+}
---
+++ Repository/Repository/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Repository.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public string Year { get; set; }
+    }
+}
